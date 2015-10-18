ab96380eaddbfbfaf6c7abe8f4d2b4b9b67f694b
Shyam Guthikonda <shyamguth@gmail.com>
2015-10-18 15:43:08 -0700

    Import.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '0.0.0'
+
+setup(
+    name = 'bud-get',
+    version = version,
+    description = 'Budget data utilities.',
+    url = 'http://github.com/doggan/bud-get',
+    license = 'Unlicense',
+    author='Shyam Guthikonda',
+
+    packages = ['bud_get']
+)
