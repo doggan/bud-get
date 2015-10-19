99b43b5db58c80b4e8444094cdccd80acc3da23e
Shyam Guthikonda <shyamguth@gmail.com>
2015-10-18 17:42:11 -0700

    Fix pylint warnings.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
-version = '0.0.0'
+VERSION = '0.0.0'
 
 setup(
-    name = 'bud-get',
-    version = version,
-    description = 'Budget data utilities.',
-    url = 'http://github.com/doggan/bud-get',
-    license = 'Unlicense',
+    name='bud-get',
+    version=VERSION,
+    description='Budget data utilities.',
+    url='http://github.com/doggan/bud-get',
+    license='Unlicense',
     author='Shyam Guthikonda',
 
-    packages = ['bud_get'],
-    entry_points = """
+    packages=['bud_get'],
+    entry_points="""
     [console_scripts]
     bud-get = bud_get.cli:main
     """
